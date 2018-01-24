eea90bdc312a341890a9f78580f87d571f81644c
User <User@DESKTOP-EL4KC8I>
2018-01-24 09:23:33 +0900

    MemberController 수정

--- Gtu_solution/src/controller/MemberController.java
+++ Gtu_solution/src/controller/MemberController.java
@@ -11,9 +11,11 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MemberController {
 
 	
